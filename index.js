b8847f7a11829747c69ae56719df3abedc6835ea
Phil <phil.github@gmail.com>
2024-04-07 18:21:30 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  const button = document.getElementById("button");
+  button.addEventListener("click", function () {
+    alert("I was clicked");
+  });
 }
+
+addingEventListener();
